c22add32026de6c8c7e307e5bcf5b2c78b5343fa
Camille Reverdy <kylas07@gmail.com>
2024-10-07 15:24:30 +0200

    add

--- src/main/java/fr/m2i/api/controllers/HelloController.java
+++ src/main/java/fr/m2i/api/controllers/HelloController.java
@@ -9,4 +9,10 @@ public class HelloController {
     public String getHello() {
         return "Hello World";
     }
+    
+    @GetMapping("/")
+    public String getHome() {
+        return "Home Page";
+    }
+
 }
